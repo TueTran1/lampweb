eeade6833065cf58c800ff58c38c22bc4d0ab3ac
XeusNguyen <kobikiu810@gmail.com>
2022-05-19 13:43:28 +0700

    update GPIO on rasp

--- home.py
+++ home.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    GPIO.output(17, GPIO.LOW)
+    GPIO.output(18, GPIO.LOW)
     return render_template('index.html')
 
 
@@ -14,12 +22,20 @@ def turnON():
     checkblue = request.form.get('checkblue')
     print(checkyel, checkblue)
     if checkyel == 'on' and checkblue is None:
+        GPIO.output(17, GPIO.HIGH)
+        GPIO.output(18, GPIO.LOW)
         return render_template('turnOnYel.html')
     if checkblue == 'on' and checkyel is None:
+        GPIO.output(17, GPIO.LOW)
+        GPIO.output(18, GPIO.HIGH)
         return render_template('turnOnBlue.html')
     if checkblue == 'on' and checkyel == 'on':
+        GPIO.output(17, GPIO.HIGH)
+        GPIO.output(18, GPIO.HIGH)
         return render_template('turnOnBoth.html')
     else:
+        GPIO.output(17, GPIO.LOW)
+        GPIO.output(18, GPIO.LOW)
         return render_template('index.html')
     
 app.run(debug=True)
